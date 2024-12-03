09a53ce119d0db151e4fc86558ae8020d63979d1
Ian Pollack <ianjpollack@gmail.com>
2024-12-02 16:31:44 -0800

    updated new resume for desktop / update new tab favicon

--- components/Header.jsx
+++ components/Header.jsx
@@ -292,7 +292,7 @@ export default function MenuAppBar() {
           className={clsx(classes.linkButtons, classes.aniFourth, classes.border)}
           componenttype="button"
           variant="outlined"
-          href="/documents/IanPollackSoftwareEngineerResume2024.pdf"
+          href="/documents/IanPollackSoftwareEngineerResume-3-Fall2024.pdf"
           target="_blank"
         >
           Resume
